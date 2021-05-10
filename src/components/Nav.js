e71431c9ab3e8d5497af8825454298a3d2eaf177
Yangyan Lisi <yangyanddlisi@gmail.com>
2021-05-10 12:23:31 -0400

    Close nav bar once clicked

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap'; //change to react material ui
 import logo from '../images/logo.png';
 const NavTop = () => {
+    const [expanded, setExpanded] = useState(false);
     return(
-        <Navbar className="navBar" collapseOnSelect expand="xl" variant="dark" fixed="top" >
-            <Link to="/">
+        <Navbar className="navBar" expanded={expanded} expand="xl" variant="dark" fixed="top" >
+            <Link to="/" onClick={() => setExpanded(false)}>
                 <Navbar.Brand>
                     <img
                         alt="logo"
@@ -15,13 +16,13 @@ const NavTop = () => {
                     Torrin &amp; Yan
                 </Navbar.Brand>
             </Link>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded? false: "expanded")} />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Link className="nav-link" to="/about">About</Link>
-                    <Link className="nav-link" to="/timeline">Timeline</Link>
-                    <Link className="nav-link" to="/covid19">Covid-19</Link>
-                    <Link className="nav-link" to="/faq">FAQ</Link>
+                    <Link className="nav-link" to="/about" onClick={() => setExpanded(false)}>About</Link>
+                    <Link className="nav-link" to="/timeline" onClick={() => setExpanded(false)}>Timeline</Link>
+                    <Link className="nav-link" to="/covid19" onClick={() => setExpanded(false)}>Covid-19</Link>
+                    <Link className="nav-link" to="/faq" onClick={() => setExpanded(false)}>FAQ</Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
