1e67d3002f80ddcbd337c8ed93253388d2a9924f
Yangyan Lisi <yangyanddlisi@gmail.com>
2021-05-24 00:39:58 -0400

    Added Rejection Count & Await Count

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,29 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Grid, TableContainer, Table, TableRow, TableHead, TableCell, TableBody } from '@material-ui/core';
+import { Typography, Grid, TableContainer, Table, TableRow, TableHead, TableCell, TableBody, CardContent, CardActions } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
 import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 
 const Admin = () => {
-    const [attendancesData, setAttendanceData] = useState([]);
+    const [attendancesData, setAttendancesData] = useState([]);
+    const [notAttendancesData, setNotAttendancesData] = useState([]);
+    const [awaitingData, setAwaitingData] = useState([]);
 
     useEffect(() => {
         const getAttendanceData = () => {
-            setAttendanceData([]);
+            setAttendancesData([]);
             let config = {
                 headers: {
                     'x-api-key': '969jrQYjlh8D9psm47ix22kCMZx7C1QC3xBaXndh'
                 }
             }
     
-            axios.get(`https://9jnxvfagb0.execute-api.us-east-1.amazonaws.com/yan/admin`, config)
+            axios.get(`https://9jnxvfagb0.execute-api.us-east-1.amazonaws.com/yan/admin?adminCondition=true`, config)
                 .then(async (res) => {
-                    if(res.data.attendancesData.length > 0) {
-                        await setAttendanceData(res.data.attendancesData);
+                    if(res.data.guestsData.length > 0) {
+                        await setAttendancesData(res.data.guestsData);
                     }
                 })
-        }
+            
+            axios.get(`https://9jnxvfagb0.execute-api.us-east-1.amazonaws.com/yan/admin?adminCondition=false`, config)
+            .then(async (res) => {
+                if(res.data.guestsData.length > 0) {
+                    await setNotAttendancesData(res.data.guestsData);
+                }
+            })
 
+            axios.get(`https://9jnxvfagb0.execute-api.us-east-1.amazonaws.com/yan/admin?adminCondition=awaiting`, config)
+            .then(async (res) => {
+                if(res.data.guestsData.length > 0) {
+                    await setAwaitingData(res.data.guestsData);
+                }
+            })
+        }
         getAttendanceData();
+
+        
+        // adminCondition = false
+        // getAttendanceData(adminCondition);
+        // adminCondition = 'no respond'
+        // getAttendanceData(adminCondition);
+
+
     }, []);
     
     return(
@@ -34,6 +59,41 @@ const Admin = () => {
                 </Grid>
             </Grid>
             <Grid container>
+                <Grid item xs={4} variant="outlined">
+                    <Card>
+                        <CardContent>
+                            <Typography color="textPrimary" gutterBottom>Total Attendance Count</Typography>
+                            <Typography variant="h5" component="h2">{attendancesData.length}</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4} variant="outlined">
+                    <Card>
+                        <CardContent>
+                            <Typography color="textPrimary" gutterBottom>Rejection Count</Typography>
+                            <Typography variant="h5" component="h2">{notAttendancesData.length}</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4} variant="outlined">
+                    <Card>
+                        <CardContent>
+                            <Typography color="textPrimary" gutterBottom>Await Count</Typography>
+                            <Typography variant="h5" component="h2">{awaitingData.length}</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+            {/* <Grid container>
                 <Grid item lg={12} className="adminBody">
                     <TableContainer>
                         <Table aria-label="customized table">
@@ -46,11 +106,11 @@ const Admin = () => {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                            {attendancesData.map((element, index) => (
+                            {guestsData.map((element, index) => (
                                 <TableRow>
-                                        <TableCell>{attendancesData[index].name}</TableCell>
-                                        <TableCell>{attendancesData[index].covid_status}</TableCell>
-                                        <TableCell>{attendancesData[index].mealname}</TableCell>
+                                        <TableCell>{guestsData[index].name}</TableCell>
+                                        <TableCell>{guestsData[index].covid_status}</TableCell>
+                                        <TableCell>{guestsData[index].mealname}</TableCell>
                                         <TableCell className="tableCell">{attendancesData[index].allergy}</TableCell>
                                 </TableRow>
                             ))}
@@ -58,7 +118,7 @@ const Admin = () => {
                         </Table>
                     </TableContainer>
                 </Grid>
-            </Grid>
+            </Grid> */}
         </div>
 
     );
